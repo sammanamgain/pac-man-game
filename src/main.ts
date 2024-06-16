8179d49224a31b7222943156bc8ec5efec9455d1
sammanamgain <lionalmessi609@gmail.com>
2024-06-16 20:47:20 +0545

    collision detected and movement improved

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,15 @@ canvas.width = canvasWidth;
 canvas.height = canvasHeight;
 
 
-const grid: string[][] = [['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
-	['_', '', '', '', '', '', '', '', '', '_'],
-	['_', '', '', '', '', 'p', '', '', '_', '_'],
-	['_', '', '', '', '', '', '', '', '', '_'],
-	['_', '', '', '', '', '', '', '', '', '_'],
-	['_', '_', '_', '_', '_', '_', '_', '_', '_', '_']]
+const grid: string[][] = [['_', '_', '_', '_', '_', '_',  '_'],
+
+	['_', '', '', '', 'p', '',  '_',],
+	['_', '', '_', '',  '_', '', '_',],
+	['_', '', '', '',  '', '', '_',],
+
+	['_', '', '_', '',  '_', '', '_',],
+	['_', '', '', '',  '', '', '_',],
+	['_',  '_', '_', '_', '_', '_', '_',]]
 const walls: Wall[] = []
 let player: Player;
 grid.forEach((row: string[], i: number) => {
@@ -25,7 +28,7 @@ grid.forEach((row: string[], i: number) => {
 				walls.push(new Wall({position: new Position(50 * j, 50 * i), height: 50, width: 50}))
 				break;
 			case  'p':
-				player = new Player({position: new Position(50 * j + 25, 50 * i + 25), radius: 24})
+				player = new Player({position: new Position(50 * j + 25, 50 * i + 25), radius: 20})
 		}
 	})
 })
@@ -33,35 +36,76 @@ grid.forEach((row: string[], i: number) => {
 
 function animate(): void {
 	ctx.clearRect(0, 0, canvasWidth, canvasHeight)
-	player.vx = 0
-	player.vy = 0
+
 	// we need to use lastKey as when clicking two keys at once, we need to find the lastKey pressed
 	// suppose I am clicking w , then I pressed s with keeping w pressed ,as w is already pressed,it won't reach to elseif "s" block
 
+
+	// checking collision in next frame in advanced
 	if (keys.w.pressed && keys.lastKey === 'w') {
-		player.vy = -5
+
+		for (let i = 0; i < walls.length; i++) {
+			const wall: Wall = walls[i]
+			if (checkCollision({...player, vy: -3}, wall)) {
+				player.vy = 0
+				break;
+			}
+			else {
+				player.vy = -3
+			}
+
+
+		}
+
 	}
 	else if (keys.s.pressed && keys.lastKey === 's') {
-		player.vy = 5
+
+		for (let i = 0; i < walls.length; i++) {
+			const wall: Wall = walls[i]
+			if (checkCollision({...player, vy: 3}, wall)) {
+				player.vy = 0
+				break;
+			}
+			else {
+				player.vy = 3
+			}
+		}
 	}
 	else if (keys.a.pressed && keys.lastKey === 'a') {
-		player.vx = -5
+
+		for (let i = 0; i < walls.length; i++) {
+			const wall: Wall = walls[i]
+			if (checkCollision({...player, vx: -3}, wall)) {
+				player.vx = 0
+				break;
+			}
+			else {
+				player.vx = -3
+			}
+		}
 	}
 	else if (keys.d.pressed && keys.lastKey === 'd') {
-		player.vx = 5
+
+		for (let i = 0; i < walls.length; i++) {
+			const wall: Wall = walls[i]
+			if (checkCollision({...player, vx: 3}, wall)) {
+				player.vx = 0
+				break;
+			}
+			else {
+				player.vx = 3
+			}
+		}
 	}
 	walls.forEach((wall: Wall) => {
 		wall.draw()
+		// checking collision in current frame
 		if (checkCollision(player, wall)) {
-			console.log("collision occured")
+
 			player.vx = 0;
 			player.vy = 0;
 		}
 	})
-
-
-	console.log(player.vx, player.vy)
-
 
 	player.update()
 	player.draw()
--- src/utils/checkCollision.ts
+++ src/utils/checkCollision.ts
@@ -1,6 +1,13 @@
 import {Player} from '../class/player.ts'
 import {Wall} from '../class/wall.ts';
-export function checkCollision(circle:Player,rectangle:Wall):boolean
+import {Position} from "../class/position.ts";
+interface playerObject{
+vy:number,
+	position:Position,
+	radius:number,
+	vx:number
+}
+export function checkCollision(circle:Player|playerObject,rectangle:Wall):boolean
 {
 	return (circle.position.y - circle.radius + circle.vy<=
 		rectangle.position.y + rectangle.height &&
